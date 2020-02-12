d9840f96e91fb4e5d933b369e8ac98731c7cc5cc
hedgehogus <research162@gmail.com>
2020-02-13 00:14:19 +0300

    54

--- npm/app.js
+++ npm/app.js
@@ -1,1 +1,10 @@
-const bcrypt = require('bcrypt.js');
+const bcrypt = require('bcryptjs');
+
+var salt = bcrypt.genSaltSync(10);
+var hash = bcrypt.hashSync("qwerty", salt);
+
+const match = bcrypt.compareSync("QWERTY", hash);
+const match2 = bcrypt.compareSync("qwerty", hash);
+
+console.log(hash, match);
+console.log(match2);
--- npm/hints.md
+++ npm/hints.md
@@ -2,3 +2,6 @@ npm install -h  **quick help -h**
 
 - alternative : isntall or i
 
+npm install http-server -g  **install globally**
+
+npm ls -g --depth=0  **display main global packages**
