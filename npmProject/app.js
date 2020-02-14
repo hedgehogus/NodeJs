493094cf107d8602aa81b7b3f931c2c0f6304355
hedgehogus <research162@gmail.com>
2020-02-14 22:37:26 +0300

    61

--- npmProject/app.js
+++ npmProject/app.js
@@ -1,11 +1,36 @@
 const express = require('express');
+const todos = require('./todos');
 
 const app = express();
 
 app.get('/', (req, res) => {
     // res.send('<h1>express</h1>'); //instead of res.end for correct headers
 
-    res.json({title: 'express'}); // we can use res.json() instead of res.send if we sure we senging json
+    // res.json({title: 'express'}); // we can use res.json() instead of res.send if we sure we senging json
+
+    res.send('express Todo');
+});
+
+app.get('/todos', (req, res) => {
+    // console.log(req.query);
+
+    if(req.query.completed) {
+        return res.json(todos.filter(todo => todo.completed.toString() === req.query.completed));
+    }
+
+    res.json(todos);
+});
+
+app.get('/todos/:id', (req, res) => {
+    // console.log(req.params);
+
+    let todo = todos.find(todo => todo.id == req.params.id);
+
+    // if(!todo) res.sendStatus(404); //automatic response text
+
+    if (!todo) res.status(404).send('todo not found');
+
+    res.json(todo);
 })
 
 app.listen(5000, () => console.log('server is working'));
