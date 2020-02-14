4dc1af687045f6944f30eb2749a16065199d31cc
hedgehogus <research162@gmail.com>
2020-02-14 22:03:32 +0300

    60

--- npmProject/app.js
+++ npmProject/app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    // res.send('<h1>express</h1>'); //instead of res.end for correct headers
+
+    res.json({title: 'express'}); // we can use res.json() instead of res.send if we sure we senging json
+})
+
+app.listen(5000, () => console.log('server is working'));
